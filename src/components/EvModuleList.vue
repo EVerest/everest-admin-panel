d90bd9c02eb971ab6b9f3f801dcb5484adf0c6cf
Philip Molares <philip.molares@pionix.de>
2023-12-15 11:06:15 +0100

    feat: warn about overriding config on load
    Instead of just doing it, the user is asked if they want to overwrite the current config and given the option to cancel their action.
    
    Signed-off-by: Philip Molares <philip.molares@pionix.de>

--- src/components/EvModuleList.vue
+++ src/components/EvModuleList.vue
@@ -25,17 +25,41 @@
     </v-expansion-panel>
     <v-expansion-panel :disabled="config_list.length == 0">
       <v-expansion-panel-header>
-        {{ config_list.length == 0 ? "No configs available" : "Available configs" }}</v-expansion-panel-header
-      >
+        {{ config_list.length == 0 ? "No configs available" : "Available configs" }}
+      </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-sheet class="d-flex align-center my-1 pa-2" elevation="1" v-for="config in config_list" :key="config">
           {{ config }}
           <v-spacer />
-          <v-btn icon @click="load_config(config)">
+          <v-btn icon @click="check_if_config_can_be_loaded(config)">
             <v-icon>mdi-upload</v-icon>
           </v-btn>
         </v-sheet>
       </v-expansion-panel-content>
+      <v-dialog
+          v-model="dialog"
+          width="auto"
+      >
+        <v-card>
+          <v-card-title class="d-flex flex-row align-baseline">
+            <v-icon
+              size="large"
+              color="error"
+            >
+              mdi-alert-circle
+            </v-icon>
+            &nbsp;
+            Warning
+          </v-card-title>
+          <v-card-text>
+            <p>Do you want to discard the current config and load the new one?</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="error" @click="load_config(config_to_load)">Load config</v-btn>
+            <v-btn @click="dialog = false">Don't load config</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-expansion-panel>
     <v-expansion-panel>
       <v-expansion-panel-header> Issue commands </v-expansion-panel-header>
@@ -56,6 +80,10 @@
 import Vue from "vue";
 import EVConfigModel from "@/modules/evbc/config_model";
 
+type Data = {
+  dialog: boolean;
+  config_to_load: string | null
+}
 export default Vue.extend({
   computed: {
     current_config(): EVConfigModel {
@@ -72,6 +100,10 @@ export default Vue.extend({
       return Object.entries(configs).map(([key]) => key);
     },
   },
+  data: (): Data => ({
+    dialog: false,
+    config_to_load: null,
+  }),
   methods: {
     add_module_to_config(type: string) {
       // FIXME (aw): does this logic belongs to here?
@@ -83,7 +115,16 @@ export default Vue.extend({
         this.$store.dispatch("evbc/set_opened_config", new_config);
       }
     },
+    check_if_config_can_be_loaded(name: string) {
+      if (!this.current_config) {
+        this.load_config(name);
+        return;
+      }
+      this.config_to_load = name;
+      this.dialog = true;
+    },
     load_config(name: string) {
+      this.dialog = false;
       const new_config = this.$evbc.load_config(name);
       this.$store.dispatch("evbc/set_opened_config", new_config);
     },
