629fba157dae0dcbdc8d606f169a3508561ad47d
Philip Molares <philip.molares@udo.edu>
2024-02-19 17:44:49 +0100

    feat: warn about overriding config on load (#7)
    Instead of just doing it, the user is asked if they want to overwrite the current config and given the option to cancel their action.
    
    Signed-off-by: Philip Molares <philip.molares@pionix.de>
    Co-authored-by: Philip Molares <philip.molares@pionix.de>

--- src/components/EvModuleList.vue
+++ src/components/EvModuleList.vue
@@ -25,17 +25,26 @@
     </v-expansion-panel>
     <v-expansion-panel :disabled="config_list.length == 0">
       <v-expansion-panel-header>
-        {{ config_list.length == 0 ? "No configs available" : "Available configs" }}</v-expansion-panel-header
-      >
+        {{ config_list.length == 0 ? "No configs available" : "Available configs" }}
+      </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-sheet class="d-flex align-center my-1 pa-2" elevation="1" v-for="config in config_list" :key="config">
           {{ config }}
           <v-spacer />
-          <v-btn icon @click="load_config(config)">
+          <v-btn icon @click="load_config_if_empty(config)">
             <v-icon>mdi-upload</v-icon>
           </v-btn>
         </v-sheet>
       </v-expansion-panel-content>
+      <ev-dialog
+          :show_dialog="show_dialog"
+          title="Warning"
+          text="Do you want to discard the current config and load the new one?"
+          accept_text="Load config"
+          deny_text="Don't load config"
+          @accept="load_config(config_to_load)"
+          @deny="close_dialog()"
+      />
     </v-expansion-panel>
     <v-expansion-panel>
       <v-expansion-panel-header> Issue commands </v-expansion-panel-header>
@@ -55,8 +64,14 @@
 <script lang="ts">
 import Vue from "vue";
 import EVConfigModel from "@/modules/evbc/config_model";
+import EvDialog from "@/components/EvDialog.vue";
 
+type Data = {
+  show_dialog: boolean;
+  config_to_load: string | null;
+}
 export default Vue.extend({
+  components: {EvDialog},
   computed: {
     current_config(): EVConfigModel {
       return this.$store.getters["evbc/current_config"];
@@ -72,6 +87,10 @@ export default Vue.extend({
       return Object.entries(configs).map(([key]) => key);
     },
   },
+  data: (): Data => ({
+    show_dialog: false,
+    config_to_load: null,
+  }),
   methods: {
     add_module_to_config(type: string) {
       // FIXME (aw): does this logic belongs to here?
@@ -83,12 +102,24 @@ export default Vue.extend({
         this.$store.dispatch("evbc/set_opened_config", new_config);
       }
     },
+    load_config_if_empty(name: string) {
+      if (!this.current_config) {
+        this.load_config(name);
+        return;
+      }
+      this.config_to_load = name;
+      this.show_dialog = true;
+    },
     load_config(name: string) {
+      this.show_dialog = false;
       const new_config = this.$evbc.load_config(name);
       this.$store.dispatch("evbc/set_opened_config", new_config);
     },
     execute(command: string) {
       this.$evbc.execute_remote_command(command);
+    },
+    close_dialog() {
+      this.show_dialog = false;
     },
   },
 });
