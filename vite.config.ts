fbebaaac88c2fabf55bf24c69ac926b0eb315c5b
Lukas Mertens <git@lukas-mertens.de>
2024-02-19 17:50:51 +0100

    ci: github pages setup
    Signed-off-by: Lukas Mertens <git@lukas-mertens.de>

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,49 @@
 import Vue from '@vitejs/plugin-vue';
 import Vuetify, {transformAssetUrls} from 'vite-plugin-vuetify';
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import ViteFonts from 'unplugin-fonts/vite';
 import {commonjsDeps} from '@koumoul/vjsf/utils/build';
 import {fileURLToPath} from "node:url";
 import commonjs from "@rollup/plugin-commonjs";
 
-export default defineConfig({
-    optimizeDeps: {
-        include: commonjsDeps,
-    },
-    build: {
-        commonjsOptions: {
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        base: mode === 'pages' ? `/everest-admin-panel/${process.env.SUBDIR}` : '',
+        optimizeDeps: {
             include: commonjsDeps,
-        }
-    },
-    plugins: [
-        commonjs(),
-        Vue({
-            template: { transformAssetUrls }
-        }),
-        Vuetify({
-            autoImport: true,
-        }),
-        ViteFonts({
-            google: {
-                families: [ {
-                    name: 'Roboto',
-                    styles: 'wght@100;300;400;500;700;900',
-                }],
-            },
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-    },
-    server: {
-        port: 8080,
-    },
+        build: {
+            commonjsOptions: {
+                include: commonjsDeps,
+            }
+        },
+        plugins: [
+            commonjs(),
+            Vue({
+                template: { transformAssetUrls }
+            }),
+            Vuetify({
+                autoImport: true,
+            }),
+            ViteFonts({
+                google: {
+                    families: [ {
+                        name: 'Roboto',
+                        styles: 'wght@100;300;400;500;700;900',
+                    }],
+                },
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+        },
+        server: {
+            port: 8080,
+        },
+
+    }
 });
