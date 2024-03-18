115b5d150ddea02471234b18771c3b79eee959b7
lukas-mertens <lukas-mertens@users.noreply.github.com>
2024-03-18 14:14:32 +0000

    Deploying to gh-pages

---
+++ pr-87/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/everest-admin-panel/pr-87/sw.js', { scope: '/everest-admin-panel/pr-87/' })})}
