17f2b7640a1da08f6ba9c374cdb4074af0308ee9
lukas-mertens <lukas-mertens@users.noreply.github.com>
2024-03-19 11:00:31 +0000

    Deploying to gh-pages

--- pr-88/assets/index-nqwasZkw.js
+++ pr-88/assets/index-nqwasZkw.js
@@ -9,7 +9,7 @@ var __publicField = (obj, key, value) => {
   return value;
 };
 var require_index_001 = __commonJS({
-  "assets/index-TSXhZweo.js"(exports, module) {
+  "assets/index-nqwasZkw.js"(exports, module) {
     var _a;
     (function polyfill() {
       const relList = document.createElement("link").relList;
@@ -81314,7 +81314,6 @@ Reason: ${error2}`);
         this._socket.close();
       }
       async issue_rpc(method, params, notification) {
-        console.log("Issuing RPC request", method, params, notification);
         const id2 = notification ? void 0 : Math.floor(Math.random() * 1024 * 1024);
         const rpc_request = {
           method,
