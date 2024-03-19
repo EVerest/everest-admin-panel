4e5deddddc27fade923d0833ea029b0bd98cc1b7
lukas-mertens <lukas-mertens@users.noreply.github.com>
2024-03-19 11:04:40 +0000

    Deploying to gh-pages

--- pr-94/assets/index-6zyBVR5o.js
+++ pr-94/assets/index-6zyBVR5o.js
@@ -9,7 +9,7 @@ var __publicField = (obj, key, value) => {
   return value;
 };
 var require_index_001 = __commonJS({
-  "assets/index-UVrcDES9.js"(exports, module) {
+  "assets/index-6zyBVR5o.js"(exports, module) {
     var _a;
     (function polyfill() {
       const relList = document.createElement("link").relList;
@@ -81396,7 +81396,6 @@ Reason: ${error2}`);
         this._socket.close();
       }
       async issue_rpc(method, params, notification) {
-        console.log("Issuing RPC request", method, params, notification);
         const id2 = notification ? void 0 : Math.floor(Math.random() * 1024 * 1024);
         const rpc_request = {
           method,
