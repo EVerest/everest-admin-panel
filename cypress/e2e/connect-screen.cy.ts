ee1dd1455aa2aab420b19294c4f0ebc0e4eee4a9
Lukas Mertens <git@lukas-mertens.de>
2024-04-03 00:37:08 +0200

    chore(license-headers): fixed again
    Signed-off-by: Lukas Mertens <git@lukas-mertens.de>

--- cypress/e2e/connect-screen.cy.ts
+++ cypress/e2e/connect-screen.cy.ts
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2024 Pionix GmbH and Contributors to EVerest
 import {faker} from "@faker-js/faker";
 
 describe('Connect-Screen', () => {
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2024 Pionix GmbH and Contributors to EVerest
 // ***********************************************************
 // This example support/e2e.ts is processed and
 // loaded automatically before your test files.
