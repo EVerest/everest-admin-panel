1896e66de30827a8626e1c1ed28d30622be3c46f
Lukas Mertens <git@lukas-mertens.de>
2024-04-10 16:05:38 +0200

    test(e2e): added config-management test
    Signed-off-by: Lukas Mertens <git@lukas-mertens.de>
    
    commit-id:2f10d1b6

---
+++ cypress/e2e/config-management.cy.ts
@@ -0,0 +1,12 @@
+import {faker} from "@faker-js/faker";
+
+describe("ConfigManagement", () => {
+    it('should be possible to create and save a new config', () => {
+        cy.connectToSimulator();
+        const configName = faker.word.words(1);
+        cy.createConfig(configName);
+        cy.get('#config-save-button').click();
+        cy.get('[data-cy="configs-expansion-panel"]').click();
+        cy.get('[data-cy="config-list-item"]').contains(configName).should('be.visible');
+    });
+});
