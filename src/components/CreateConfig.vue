5e4e593b1274c64bd6e09b16e7c98fadde521261
Lukas Mertens <git@lukas-mertens.de>
2024-04-15 14:09:34 +0200

    feat(load-config): added upload config feature
    Closes #107
    
    Signed-off-by: Lukas Mertens <git@lukas-mertens.de>
    
    commit-id:0bb94152

--- src/components/CreateConfig.vue
+++ src/components/CreateConfig.vue
@@ -8,6 +8,19 @@
         <v-btn color="default"
                variant="flat"
                density="compact"
+               icon="mdi-upload"
+               data-cy="upload-config-btn"
+               v-bind="props"
+               @click="uploadConfigPrompt()"
+        ></v-btn>
+      </template>
+      <span>Upload Config</span>
+    </v-tooltip>
+    <v-tooltip location="right" open-delay="500" v-if="state == ComponentStates.DEFAULT">
+      <template v-slot:activator="{ props }">
+        <v-btn color="default"
+               variant="flat"
+               density="compact"
                icon="mdi-plus"
                data-cy="plus-create-config-btn"
                v-bind="props"
@@ -24,7 +37,7 @@
                data-cy="abort-create-config-btn"
                icon="mdi-close"
                v-bind="props"
-               @click="abortConfigCreation()"
+               @click="resetDialog()"
         ></v-btn>
       </template>
       <span>Abort</span>
@@ -38,7 +51,7 @@
                data-cy="accept-create-config-btn"
                v-bind="props"
                :disabled="!configNameValid"
-               @click="createConfig()"
+               @click="onAcceptBtnClick()"
         ></v-btn>
       </template>
       <span>Create Config</span>
@@ -52,16 +65,31 @@
       placeholder="config name"
       :rules="[validateConfigName]"
   ></v-text-field>
+  <v-dialog v-model="showErrorDialog" @click:outside="resetDialog()">
+    <v-card color="danger">
+      <v-card-title>Couldn't load config</v-card-title>
+      <v-card-text>
+        <pre><code>{{ errors }}</code></pre>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="resetDialog()">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
 import {computed, ref} from "vue";
 import {useEvbcStore} from "@/store/evbc";
 import {storeToRefs} from "pinia";
+import yaml from "js-yaml";
+import Ajv from "ajv";
+import {EverestConfig} from "@/modules/evbc";
+import {urlToPublicAsset} from "@/utils";
 
 enum ComponentStates {
-    DEFAULT,
-    ASK_USER_FOR_CONFIG_NAME,
+  DEFAULT,
+  ASK_USER_FOR_CONFIG_NAME,
   }
 
 const evbcStore = useEvbcStore();
@@ -69,21 +97,49 @@ const evbcStore = useEvbcStore();
   const configName = ref<string>("");
   const configNameValid = computed<boolean>(() => validateConfigName() === true);
   const emit = defineEmits<{
-    createConfig: [name: string],
+    createConfig: [name: string, content?: EverestConfig],
   }>();
 const {available_configs} = storeToRefs(evbcStore);
+const configContent = ref<EverestConfig>(null);
+const errors = ref<string>(null);
+const showErrorDialog = computed<boolean>(() => !!errors.value);
 
-  function createConfig() {
+function onAcceptBtnClick() {
     if (validateConfigName() === true) {
-      emit("createConfig", configName.value);
-      state.value = ComponentStates.DEFAULT;
-      configName.value = "";
+      emit("createConfig", configName.value, configContent.value ?? undefined);
+      resetDialog();
     }
   }
 
-  function abortConfigCreation() {
+function resetDialog() {
     state.value = ComponentStates.DEFAULT;
     configName.value = "";
+    configContent.value = null;
+    errors.value = null;
+}
+
+function uploadConfigPrompt() {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".json,.yaml,.yml";
+  input.click();
+  input.onchange = (e) => {
+    const file = (e.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const parseResult = await parseConfig(e.target?.result as string);
+        if (!parseResult.errors) {
+          configContent.value = parseResult.config;
+          configName.value = file.name.replace(/\.[^.]+$/, ""); // remove file extension
+          state.value = ComponentStates.ASK_USER_FOR_CONFIG_NAME;
+        } else {
+          errors.value = parseResult.errors;
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
   }
 
   /**
@@ -102,6 +158,46 @@ const {available_configs} = storeToRefs(evbcStore);
       return true;
     }
   }
+
+/**
+ * Validates that the config content is a valid JSON or YAML config
+ */
+async function validateConfigContent(content: object): Promise<true | string> {
+  const ajv = new Ajv();
+  const schema = await getConfigJsonSchema();
+  const validate = ajv.compile(schema);
+  const valid = validate(content);
+  if (valid) {
+    return true;
+  } else {
+    return JSON.stringify(validate.errors, null, 2);
+  }
+}
+
+async function getConfigJsonSchema(): Promise<object> {
+  const response = await fetch(urlToPublicAsset('schemas/config.json'));
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json();
+}
+
+/**
+ * Parse config
+ */
+async function parseConfig(content: string): Promise<{ errors: string, config: EverestConfig }> {
+  try {
+    const config = yaml.load(content);
+    const validationResult = await validateConfigContent(config);
+    if (validationResult === true) {
+      return {errors: null, config: config as EverestConfig};
+    } else {
+      return {errors: validationResult, config: null};
+    }
+  } catch (e) {
+    return {errors: e.toString(), config: null};
+  }
+}
 </script>
 
 
