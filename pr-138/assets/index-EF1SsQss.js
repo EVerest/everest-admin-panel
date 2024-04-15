8578ba49346ea5ef4b8d0e0440fe686b7adcd737
lukas-mertens <lukas-mertens@users.noreply.github.com>
2024-04-15 07:23:06 +0000

    Deploying to gh-pages

--- pr-138/assets/index-EF1SsQss.js
+++ pr-138/assets/index-EF1SsQss.js
@@ -9,7 +9,7 @@ var __publicField = (obj, key, value) => {
   return value;
 };
 var require_index_001 = __commonJS({
-  "assets/index-77q5i1sS.js"(exports, module) {
+  "assets/index-EF1SsQss.js"(exports, module) {
     var _a;
     (function polyfill() {
       const relList = document.createElement("link").relList;
@@ -48859,7 +48859,7 @@ Reason: ${error2}`);
     })(ajv, ajv.exports);
     var ajvExports = ajv.exports;
     const Ajv$2 = /* @__PURE__ */ getDefaultExportFromCjs(ajvExports);
-    const _withScopeId = (n) => (pushScopeId("data-v-8c45851a"), n = n(), popScopeId(), n);
+    const _withScopeId = (n) => (pushScopeId("data-v-a7df99fb"), n = n(), popScopeId(), n);
     const _hoisted_1$4 = { class: "btn-container" };
     const _hoisted_2$3 = /* @__PURE__ */ _withScopeId(() => /* @__PURE__ */ createBaseVNode("span", null, "Create Config", -1));
     const _hoisted_3$1 = /* @__PURE__ */ _withScopeId(() => /* @__PURE__ */ createBaseVNode("span", null, "Create Config", -1));
@@ -48943,7 +48943,7 @@ Reason: ${error2}`);
           }
         }
         async function getConfigJsonSchema() {
-          const response = await fetch("/public/schemas/config.json");
+          const response = await fetch(`${"/everest-admin-panel/pr-138"}/public/schemas/config.json`);
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
           }
@@ -49106,7 +49106,7 @@ Reason: ${error2}`);
         };
       }
     });
-    const CreateConfig = /* @__PURE__ */ _export_sfc(_sfc_main$z, [["__scopeId", "data-v-8c45851a"]]);
+    const CreateConfig = /* @__PURE__ */ _export_sfc(_sfc_main$z, [["__scopeId", "data-v-a7df99fb"]]);
     let evbcStore;
     let evbc;
     let notyf;
