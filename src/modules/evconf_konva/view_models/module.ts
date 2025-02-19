35007d1c17c2fe6db8256afb40e0b71de1220b0e
12fab4 <mailfuerfabian@web.de>
2025-02-19 16:30:42 +0100

    chore: fixed all linting issues
    Signed-off-by: 12fab4 <mailfuerfabian@web.de>

--- src/modules/evconf_konva/view_models/module.ts
+++ src/modules/evconf_konva/view_models/module.ts
@@ -10,7 +10,7 @@ import {
   TerminalType,
 } from "@/modules/evbc";
 import EVConfigModel from "@/modules/evbc/config_model";
-import ConfigStageContext, { ConfigStageContextEvent } from "../stage_context";
+import ConfigStageContext, { ConfigStageContextEvent, } from "../stage_context";
 
 type TerminalDistribution = Record<TerminalAlignment, number[]>;
 type TerminalLookup = Array<{
@@ -56,27 +56,27 @@ export default class ModuleViewModel {
   _observers: ViewModelChangeHandler[] = [];
   readonly _stage_context: ConfigStageContext;
 
-  constructor(model: EVConfigModel, id: ModuleInstanceID, stage_context: ConfigStageContext) {
+  constructor(model: EVConfigModel, id: ModuleInstanceID, stage_context: ConfigStageContext,) {
     this._instance_id = id;
     this._config_model = model;
     this._stage_context = stage_context;
-    this._module_instance = model.get_module_instance(id);
-    this._initialize_terminals(this._module_instance.view_config.terminals);
+    this._module_instance = model.get_module_instance(id,);
+    this._initialize_terminals(this._module_instance.view_config.terminals,);
     this._grid_position = this._module_instance.view_config.position;
     this.type = this._module_instance.type;
 
-    stage_context.add_observer((ev) => this._handle_stage_context_event(ev));
-    model.add_observer((ev) => {
-      if (ev.type == "MODULE_INSTANCE_UPDATED" && ev.id == id) {
-        this._notify({ type: "MODULE_MODEL_UPDATE" });
+    stage_context.add_observer((ev,) => this._handle_stage_context_event(ev,),);
+    model.add_observer((ev,) => {
+      if (ev.type === "MODULE_INSTANCE_UPDATED" && ev.id === id) {
+        this._notify({ type: "MODULE_MODEL_UPDATE", },);
       }
-    });
+    },);
   }
 
-  _initialize_terminals(terminal_arrangement: TerminalArrangement) {
-    Object.entries(terminal_arrangement).forEach(([_alignment, terminals]) => {
+  _initialize_terminals(terminal_arrangement: TerminalArrangement,) {
+    Object.entries(terminal_arrangement,).forEach(([ _alignment, terminals, ],) => {
       const alignment = _alignment as TerminalAlignment;
-      terminals.forEach((terminal) => {
+      terminals.forEach((terminal,) => {
         const index = this.terminal_dist[alignment].length;
         const terminal_id = this.terminal_lookup.length;
 
@@ -84,43 +84,43 @@ export default class ModuleViewModel {
           terminal,
           alignment,
           index,
-        });
+        },);
 
-        this.terminal_dist[alignment].push(terminal_id);
-      });
-    });
+        this.terminal_dist[alignment].push(terminal_id,);
+      },);
+    },);
   }
 
-  add_observer(handler: ViewModelChangeHandler) {
-    this._observers.push(handler);
+  add_observer(handler: ViewModelChangeHandler,) {
+    this._observers.push(handler,);
 
     return () => {
-      this._observers = this._observers.filter((other) => other !== handler);
+      this._observers = this._observers.filter((other,) => other !== handler,);
     };
   }
 
-  notify_stage_context(event: ConfigStageContextEvent) {
-    this._stage_context._publish(event)
+  notify_stage_context(event: ConfigStageContextEvent,) {
+    this._stage_context._publish(event,);
   }
 
-  _notify(ev: ViewModelChangeEvent) {
-    this._observers.forEach((handler) => handler(ev));
+  _notify(ev: ViewModelChangeEvent,) {
+    this._observers.forEach((handler,) => handler(ev,),);
   }
 
-  clicked_terminal(terminal_id: number) {
+  clicked_terminal(terminal_id: number,) {
     const terminal = this.terminal_lookup[terminal_id].terminal;
-    this._stage_context.clicked_terminal(terminal, this._instance_id);
+    this._stage_context.clicked_terminal(terminal, this._instance_id,);
   }
 
   clicked_title() {
-    this._stage_context.clicked_instance(this._instance_id);
+    this._stage_context.clicked_instance(this._instance_id,);
   }
 
-  set_cursor(type: string) {
+  set_cursor(type: string,) {
     this._stage_context.container.style.cursor = type;
   }
 
-  _handle_stage_context_event(event: ConfigStageContextEvent) {
+  _handle_stage_context_event(event: ConfigStageContextEvent,) {
     if (event.type !== "SELECT") {
       return;
     }
@@ -136,26 +136,26 @@ export default class ModuleViewModel {
 
       const terminal = selection_event.terminal;
 
-      this.terminal_lookup.forEach((item, id) => {
+      this.terminal_lookup.forEach((item, id,) => {
         if (
           item.terminal.type !== terminal.type &&
           (terminal.type === "provide"
-            ? this._config_model.interfaces_match(terminal.interface, item.terminal.interface)
-            : this._config_model.interfaces_match(item.terminal.interface, terminal.interface))
+            ? this._config_model.interfaces_match(terminal.interface, item.terminal.interface,)
+            : this._config_model.interfaces_match(item.terminal.interface, terminal.interface,))
         ) {
           return;
         }
 
-        modify_event.disable.push(id);
-      });
-      this._notify(modify_event);
+        modify_event.disable.push(id,);
+      },);
+      this._notify(modify_event,);
     } else {
       this._notify({
         type: "TERMINAL_MODIFY_APPEARENCE",
-        normal: Array.from(this.terminal_lookup.keys()),
+        normal: Array.from(this.terminal_lookup.keys(),),
         disable: [],
         highlight: [],
-      });
+      },);
     }
   }
 
@@ -168,58 +168,58 @@ export default class ModuleViewModel {
   }
 
   // returns diff or null
-  set grid_position(pos: { x: number; y: number }) {
+  set grid_position(pos: { x: number; y: number },) {
     this._grid_position = pos;
-    this._config_model.update_module_view_position(this._instance_id, pos);
+    this._config_model.update_module_view_position(this._instance_id, pos,);
   }
 
-  move_terminal(terminal_id: number, new_align: TerminalAlignment, new_index: number): TerminalAlignment[] {
+  move_terminal(terminal_id: number, new_align: TerminalAlignment, new_index: number,): TerminalAlignment[] {
     const cur_align = this.terminal_lookup[terminal_id].alignment;
     const cur_index = this.terminal_lookup[terminal_id].index;
 
-    if (new_align != cur_align) {
+    if (new_align !== cur_align) {
       // FIXME (aw): we could do an assert here checking the terminal_id
-      this.terminal_dist[cur_align].splice(cur_index, 1);
-      this.terminal_dist[new_align].splice(new_index, 0, terminal_id);
+      this.terminal_dist[cur_align].splice(cur_index, 1,);
+      this.terminal_dist[new_align].splice(new_index, 0, terminal_id,);
 
       // set new align
       this.terminal_lookup[terminal_id].alignment = new_align;
 
       // adjust dirty positions (could be optimized)
-      this._recalculate_terminal_index([cur_align, new_align]);
-      return [cur_align, new_align];
+      this._recalculate_terminal_index([ cur_align, new_align, ],);
+      return [ cur_align, new_align, ];
     } else {
       if (new_index > cur_index + 1 || new_index < cur_index) {
         // this should be a rotate!
-        this.terminal_dist[cur_align].splice(cur_index, 1);
+        this.terminal_dist[cur_align].splice(cur_index, 1,);
         const corrected_new_index = new_index < cur_index ? new_index : new_index - 1;
-        this.terminal_dist[cur_align].splice(corrected_new_index, 0, terminal_id);
+        this.terminal_dist[cur_align].splice(corrected_new_index, 0, terminal_id,);
 
         // adjust dirty positions (could be optimized)
-        this._recalculate_terminal_index([cur_align]);
-        return [cur_align];
+        this._recalculate_terminal_index([ cur_align, ],);
+        return [ cur_align, ];
       } else {
         return [];
       }
     }
   }
 
-  get_terminal_lookup_id(name: string, type: TerminalType) {
-    return this.terminal_lookup.findIndex((item) => item.terminal.id === name && item.terminal.type === type);
+  get_terminal_lookup_id(name: string, type: TerminalType,) {
+    return this.terminal_lookup.findIndex((item,) => item.terminal.id === name && item.terminal.type === type,);
   }
 
-  _recalculate_terminal_index(alignments: TerminalAlignment[]) {
+  _recalculate_terminal_index(alignments: TerminalAlignment[],) {
     const new_arrangement = {} as TerminalArrangement;
 
-    alignments.forEach((alignment) => {
-      this.terminal_dist[alignment].forEach((terminal_id, index) => {
+    alignments.forEach((alignment,) => {
+      this.terminal_dist[alignment].forEach((terminal_id, index,) => {
         this.terminal_lookup[terminal_id].index = index;
-      });
+      },);
 
-      new_arrangement[alignment] = this.terminal_dist[alignment].map((id) => this.terminal_lookup[id].terminal);
-    });
+      new_arrangement[alignment] = this.terminal_dist[alignment].map((id,) => this.terminal_lookup[id].terminal,);
+    },);
 
     // commit these changes to our model!
-    this._config_model.update_module_view_terminals(this._instance_id, new_arrangement);
+    this._config_model.update_module_view_terminals(this._instance_id, new_arrangement,);
   }
 }
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,36 +8,36 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-import {md3} from 'vuetify/blueprints';
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { md3, } from "vuetify/blueprints";
 
 // Composables
-import {createVuetify} from 'vuetify'
+import { createVuetify, } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export const everestTheme = {
-    dark: false,
-    colors: {
-        background: "#EFEDE8",
-        surface: "#EFEDE8",
-        primary: "#254C84",
-        secondary: "#35CE7C",
-        error: "#b9352b",
-        info: "#254C84",
-        success: "#35CE7C",
-        warning: "#c09112",
-        "on-background": "#212121",
-        "on-surface": "#212121",
-    }
+  dark: false,
+  colors: {
+    background: "#EFEDE8",
+    surface: "#EFEDE8",
+    primary: "#254C84",
+    secondary: "#35CE7C",
+    error: "#b9352b",
+    info: "#254C84",
+    success: "#35CE7C",
+    warning: "#c09112",
+    "on-background": "#212121",
+    "on-surface": "#212121",
+  },
 };
 export const currentTheme = everestTheme;
 export default createVuetify({
-    blueprint: md3,
-    theme: {
-        defaultTheme: 'everestTheme',
-        themes: {
-            everestTheme,
-        }
+  blueprint: md3,
+  theme: {
+    defaultTheme: "everestTheme",
+    themes: {
+      everestTheme,
     },
-});
+  },
+},);
