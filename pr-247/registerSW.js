bae67073ea1b3c2415fe7bdd21c5d284152da31f
12fab4 <12fab4@users.noreply.github.com>
2025-03-24 15:37:09 +0000

    Deploying to gh-pages

---
+++ pr-247/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/everest-admin-panel/pr-247/sw.js', { scope: '/everest-admin-panel/pr-247/' })})}
