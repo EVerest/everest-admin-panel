6180aa15d91ba805b9aa25fd778fbc2e43d77d31
12fab4 <mailfuerfabian@web.de>
2025-04-28 10:52:59 +0200

    removed debug logging
    Signed-off-by: 12fab4 <mailfuerfabian@web.de>

--- src/store/errorStore.ts
+++ src/store/errorStore.ts
@@ -4,24 +4,15 @@
  */
 
 import { defineStore } from "pinia";
-import { computed, ref, watch } from "vue";
+import { computed, ref } from "vue";
 
 export const useErrorStore = defineStore("error", () => {
-  console.log("[Store] Initializing error store...");
   const error = ref<string | null>(null);
-  watch(error, (newValue: string | null) => {
-    console.log("Error changed:", newValue);
-  });
-  // Inside useErrorStore
   function setError(errorMessage: string) {
-    console.log("[Store] setError called with:", errorMessage); // <-- Add this
-    console.log("[Store] error.value BEFORE:", error.value); // <-- Add this
     error.value = errorMessage;
-    console.log("[Store] error.value AFTER:", error.value); // <-- Add this
   }
 
   function clearError() {
-    console.trace("[Store] clearError called");
     error.value = null;
   }
 
