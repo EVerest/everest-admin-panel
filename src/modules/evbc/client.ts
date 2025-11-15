375e0356b9a6c6cd24b2ee68e6a08b20503113bd
Patrick Atoon <patrick.atoon@gmail.com>
2025-11-15 22:38:29 +0100

    feat(i18n): Explicitly convert to string to prevent "Unsafe assignment of an error typed value." error
    Signed-off-by: Patrick Atoon <patrick.atoon@gmail.com>

--- src/modules/evbc/client.ts
+++ src/modules/evbc/client.ts
@@ -74,7 +74,7 @@ class EVBackendClient {
   // - it would be nice, if we got an object after successful connection, that contains that
   load_config(name: string) {
     if (!(name in this.evbcStore.available_configs)) {
-      throw Error(t("evbc.client.configurationNotFound", { name }));
+      throw Error(String(t("evbc.client.configurationNotFound", { name })));
     }
     const config = this.evbcStore.available_configs[name];
     return new EVConfigModel(this.everest_definitions, name, config);
@@ -98,21 +98,21 @@ class EVBackendClient {
   _connection_state_listener(status: ConnectionStatus) {
     let event: ConnectionStateEvent = null;
     if (status.type === "OPEN") {
-      event = { type: "INFO", text: t("evbc.client.openConnection", { connectionUrl: status.url }) };
+      event = { type: "INFO", text: String(t("evbc.client.openConnection", { connectionUrl: status.url })) };
     } else if (status.type === "OPENED") {
       // FIXME (aw): this state handling is not production ready yet, in fact it will be probably quite complicated
       if (!this.initialized) {
-        event = { type: "INFO", text: t("evbc.client.openConnectionSuccess") };
+        event = { type: "INFO", text: String(t("evbc.client.openConnectionSuccess")) };
         this._on_connected();
       } else {
-        event = { type: "INITIALIZED", text: t("evbc.client.reconnectedSuccess") };
+        event = { type: "INITIALIZED", text: String(t("evbc.client.reconnectedSuccess")) };
       }
     } else if (status.type === "ERROR") {
-      event = { type: "FAILED", text: t("evbc.client.connectionFailed") };
+      event = { type: "FAILED", text: String(t("evbc.client.connectionFailed")) };
     } else if (status.type === "CLOSED") {
-      event = { type: "RECONNECT", text: t("evbc.client.reconnecting") };
+      event = { type: "RECONNECT", text: String(t("evbc.client.reconnecting")) };
     } else if (status.type === "DISCONNECTED") {
-      event = { type: "IDLE", text: t("evbc.client.disconnected") };
+      event = { type: "IDLE", text: String(t("evbc.client.disconnected")) };
     }
 
     if (event) {
@@ -123,7 +123,7 @@ class EVBackendClient {
   _on_connected() {
     void this._reload_instance_data().then(() => {
       this.initialized = true;
-      this._publish("connection_state", { type: "INITIALIZED", text: t("evbc.client.initialized") });
+      this._publish("connection_state", { type: "INITIALIZED", text: String(t("evbc.client.initialized")) });
     });
   }
 
@@ -131,7 +131,9 @@ class EVBackendClient {
     this.everest_definitions.modules = await this._cxn.rpc_issuer.get_modules();
     this._publish("connection_state", {
       type: "INFO",
-      text: t("evbc.client.receivedModuleFiles", { count: Object.keys(this.everest_definitions.modules).length }),
+      text: String(
+        t("evbc.client.receivedModuleFiles", { count: Object.keys(this.everest_definitions.modules).length }),
+      ),
     });
   }
 
@@ -139,9 +141,11 @@ class EVBackendClient {
     this.everest_definitions.interfaces = await this._cxn.rpc_issuer.get_interfaces();
     this._publish("connection_state", {
       type: "INFO",
-      text: t("evbc.client.receivedInterfacesDefinitions", {
-        count: Object.keys(this.everest_definitions.interfaces).length,
-      }),
+      text: String(
+        t("evbc.client.receivedInterfacesDefinitions", {
+          count: Object.keys(this.everest_definitions.interfaces).length,
+        }),
+      ),
     });
   }
 
@@ -150,7 +154,7 @@ class EVBackendClient {
     Object.assign(this.evbcStore.available_configs, cfgs);
     this._publish("connection_state", {
       type: "INFO",
-      text: t("evbc.client.receivedConfigFiles", { count: Object.keys(cfgs).length }),
+      text: String(t("evbc.client.receivedConfigFiles", { count: Object.keys(cfgs).length })),
     });
   }
 
