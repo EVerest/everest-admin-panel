df342870e511f61406a03be276d81d772a7fdf96
Patrick Atoon <patrick.atoon@gmail.com>
2026-01-26 09:40:07 +0100

    fix: Address Codacy errors in tests by referencing Cypress definitions
    Signed-off-by: Patrick Atoon <patrick.atoon@gmail.com>

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,28 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
-// ***********************************************************
-// This example support/e2e.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+/// <reference types="cypress" />
 
-// Import commands.js using ES2015 syntax:
 import "./commands";
 
 // Hide harmless ResizeObserver errors which can cause tests to fail
 const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
 
 Cypress.on("uncaught:exception", (err: Error) => {
-  /* returning false here prevents Cypress from failing the test */
+  // returning false here prevents Cypress from failing the test
   if (resizeObserverLoopErrRe.test(err.message)) {
     return false;
   }
@@ -34,6 +20,3 @@ Cypress.on("uncaught:exception", (err: Error) => {
     return false;
   }
 });
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
